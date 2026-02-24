7748b0c2de231c4816708935f46f0d0db0a3d9e4
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2026-02-24 14:39:11 -0600

    update comments

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ import numpy as np
 import rawpy
 import matplotlib.pyplot as plt
 
-print("hello world!")
-
-
+# Post Process a RAW image 
 def display_arw_image(file_path):
     with rawpy.imread(file_path) as raw:
-
         rgb = raw.postprocess()  # convert sensor data into a standard RGB image
-        # (includes: demosaicing, WB, basic color correction)
     plt.imshow(rgb)
     plt.title('ARW Image')
     plt.axis('off')
     plt.show()
 
 
-# Example usage:
+# Test image:
 display_arw_image('.\imgs\AKG02229.ARW')
