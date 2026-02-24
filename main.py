bca806d7cd927f4839b29b4f1430ff66c67b2873
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2026-02-24 14:25:39 -0600

    Add comments

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ print("hello world!")
 
 def display_arw_image(file_path):
     with rawpy.imread(file_path) as raw:
-        rgb = raw.postprocess()
+
+        rgb = raw.postprocess()  # convert sensor data into a standard RGB image
+        # (includes: demosaicing, WB, basic color correction)
     plt.imshow(rgb)
     plt.title('ARW Image')
     plt.axis('off')
     plt.show()
+
 
 # Example usage:
 display_arw_image('.\imgs\AKG02229.ARW')
