57f7e0b7e4da1d64a7a4aae679dce5e973d0e82e
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2026-02-25 16:08:20 -0600

    Add CFA map notes

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ def display_arw_image(file_path):
     plt.axis('off')
     plt.show()
 
-# Decode & extract data
-
-
+# 1.) Decode & extract data
 def decode_arw_image(file_path):
     with rawpy.imread(file_path) as raw:
         bayer = raw.raw_image_visible.copy()  # 2D Bayer mosaic (decoded RAW data)
@@ -29,7 +27,7 @@ def decode_arw_image(file_path):
     # print("black levels:", black, "white level:", white)
     return bayer, cfa, black, white, color_desc
 
-
+# 2.) Find linear
 def linearize_bayer(bayer, black_level, white_level):
     black_scalar = float(black_level[0])
     linear = (bayer.astype(np.float32) - black_scalar) / \
@@ -37,8 +35,16 @@ def linearize_bayer(bayer, black_level, white_level):
     return np.clip(linear, 0.0, 1.0) # limit linear values between 0 and 1
 
 
+
+# 3.) Create label map
+def build_rgb_masks(bayer, cfa, color_desc):
+    # get image height and width
+    h, w = bayer.shape
+    
+	# a.) repeat 2x2 CFA tile to full image size
+    full_ids = np.tile(cfa, (h // 2 + 1, w // 2 + 1))[:h, :w]
+
+
+
 bayer, cfa, black, white, color_desc = decode_arw_image('.\imgs\AKG02229.ARW')
 linear = linearize_bayer(bayer, black, white) # each pixel is a sensor intensity fraction
-
-
-
